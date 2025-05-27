1845703abd080e3108e5621fe71a4f922580c132
Russell <75209707+rustyff12@users.noreply.github.com>
2025-05-27 16:06:59 +0200

    footer styled

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,18 @@
-
-
 function Footer() {
   return (
-    <h2>Footer goes here</h2>
-  )
+    <footer className="bg-slate-800 text-white shadow-md px-4 py-4">
+      <p className="text-xl font-bold text-center">
+        © 2025{" "}
+        <a
+          href="https://github.com/rustyff12"
+          className="hover:text-cyan-400 transition-colors"
+        >
+          Russell Frankpitt
+        </a>
+        . All rights reserved.
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
