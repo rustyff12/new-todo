89f86304537c35e07058b0426b28639e1f768f4a
Russell <75209707+rustyff12@users.noreply.github.com>
2025-05-29 13:10:39 +0200

    small UI updates, list, borders etc

--- frontend/src/components/ToDoItem.tsx
+++ frontend/src/components/ToDoItem.tsx
@@ -7,16 +7,16 @@ interface ToDoItemProps {
 function ToDoItem({ todo }: ToDoItemProps) {
   return (
     <article>
-      <h2>{todo.title}</h2>
+      <h2 className="font-bold">{todo.title}</h2>
       <p className="flex items-center gap-2">
         <span className="font-bold">Status: </span>
         {todo.completed ? (
           <span className="inline-flex items-center gap-1 text-green-600">
-            <Check className="w-4 h-4" /> Completed
+            <Check className="w-4 h-4 text-white bg-green-500" /> Completed
           </span>
         ) : (
           <span className="inline-flex items-center gap-1 text-red-600">
-            <X className="w-4 h-4" /> Not Completed
+            <X className="w-4 h-4 text-white bg-red-500" /> Not Completed
           </span>
         )}
       </p>
--- frontend/src/components/ToDoList.tsx
+++ frontend/src/components/ToDoList.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import type { ToDoItemType } from "../types";
+import { Check, X } from "lucide-react";
 
 interface ToDoListProps {
   todos: ToDoItemType[];
@@ -7,11 +8,22 @@ interface ToDoListProps {
 
 function ToDoList({ todos }: ToDoListProps) {
   return (
-    <ul>
+    <ul className="flex flex-col gap-4">
       {todos.map((todo) => (
-        <li key={todo.id}>
-          <Link to={`/todos/${todo.id}`}>
-            {todo.title} - {todo.completed ? "✅" : "❌"}
+        <li
+          key={todo.id}
+          className="group flex-grow border border-gray-300 rounded px-3 py-2 hover:bg-slate-800 cursor-pointer duration-300"
+        >
+          <Link
+            to={`/todos/${todo.id}`}
+            className="inline-flex items-center gap-1 text-slate-800 group-hover:text-white"
+          >
+            {todo.title} -{" "}
+            {todo.completed ? (
+              <Check className="w-4 h-4 text-white group-hover:bg-green-600 bg-green-500 rounded" />
+            ) : (
+              <X className="w-4 h-4 text-white group-hover:bg-red-600 bg-red-500 rounded" />
+            )}
           </Link>
         </li>
       ))}
@@ -20,3 +32,5 @@ function ToDoList({ todos }: ToDoListProps) {
 }
 
 export default ToDoList;
+
+// flex items-center gap-2
